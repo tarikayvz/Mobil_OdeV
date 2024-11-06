d79b5512a80a1f2603e62ffb44cdab52ecd12325
Tarık Buğra Ayvazoğlu <tbafb1907@gmail.com>
2024-11-06 17:04:26 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,148 +1,149 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ProductApp());
+  runApp(MyApp());
 }
 
-// Ürün Modeli
-class Product {
-  final String name;
-  final double price;
-
-  Product({required this.name, required this.price});
-}
-
-// Örnek ürün listesi
-List<Product> products = [
-  Product(name: 'Laptop', price: 1500),
-  Product(name: 'Telefon', price: 800),
-  Product(name: 'Tablet', price: 500),
-  Product(name: 'Kamera', price: 300),
-  Product(name: 'Kulaklık', price: 100),
-  // Daha fazla ürün ekleyebilirsiniz
-];
-
-// Ana Uygulama Widget’ı
-class ProductApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ürün Listesi',
+      title: 'Product App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: ProductScreen(),
+      home: ProductPage(),
     );
   }
 }
 
-// Ürün Sayfası için StatefulWidget
-class ProductScreen extends StatefulWidget {
+class ProductPage extends StatefulWidget {
   @override
-  _ProductScreenState createState() => _ProductScreenState();
+  _ProductPageState createState() => _ProductPageState();
 }
 
-class _ProductScreenState extends State<ProductScreen> {
-  int selectedIndex = -1; // Başlangıçta hiçbir ürün seçilmemiş
+class _ProductPageState extends State<ProductPage> {
+  List<Map<String, dynamic>> products = [
+    {
+      "name": "Product 1",
+      "price": 100,
+      "image": "https://via.placeholder.com/150"
+    },
+    {
+      "name": "Product 2",
+      "price": 200,
+      "image": "https://via.placeholder.com/150"
+    },
+    {
+      "name": "Product 3",
+      "price": 300,
+      "image": "https://via.placeholder.com/150"
+    },
+    {
+      "name": "Product 4",
+      "price": 400,
+      "image": "https://via.placeholder.com/150"
+    },
+    {
+      "name": "Product 5",
+      "price": 500,
+      "image": "https://via.placeholder.com/150"
+    },
+  ];
+
+  int selectedIndex = -1;
+
+  void onProductTap(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ürün Listesi'),
-      ),
+      appBar: AppBar(title: Text('Product List')),
       body: Column(
         children: [
-          _buildHorizontalListView(),
-          Expanded(child: _buildGridView()),
-        ],
-      ),
-    );
-  }
-
-  // Horizontal ListView Oluşturma
-  Widget _buildHorizontalListView() {
-    return Container(
-      height: 100,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              setState(() {
-                selectedIndex = index;
-              });
-            },
-            child: Container(
-              margin: EdgeInsets.all(8),
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: selectedIndex == index ? Colors.blue : Colors.grey[300],
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Center(
-                child: Text(
-                  products[index].name,
-                  style: TextStyle(
-                    color: selectedIndex == index ? Colors.white : Colors.black,
-                    fontWeight: FontWeight.bold,
+          // Horizontal ListView
+          Container(
+            height: 80,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () => onProductTap(index),
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 8.0),
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: selectedIndex == index
+                          ? Colors.blue
+                          : Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(child: Text(products[index]["name"])),
                   ),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  // GridView Oluşturma
-  Widget _buildGridView() {
-    return GridView.builder(
-      padding: EdgeInsets.all(8),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, // Her satırda iki öğe
-        childAspectRatio: 2, // Öğe oranı
-        mainAxisSpacing: 8,
-        crossAxisSpacing: 8,
-      ),
-      itemCount: products.length,
-      itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            setState(() {
-              selectedIndex = index;
-            });
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: selectedIndex == index
-                  ? Colors.orange[200]
-                  : Colors.grey[200],
-              border: Border.all(
-                color:
-                    selectedIndex == index ? Colors.orange : Colors.transparent,
-                width: 2,
-              ),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            padding: EdgeInsets.all(8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  products[index].name,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 4),
-                Text(
-                  '\$${products[index].price.toString()}',
-                  style: TextStyle(fontSize: 14, color: Colors.grey[700]),
-                ),
-              ],
+                );
+              },
             ),
           ),
-        );
-      },
+          SizedBox(height: 10),
+          // Grid List
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () => onProductTap(index),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: selectedIndex == index
+                          ? Colors.blueAccent
+                          : Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color:
+                            selectedIndex == index ? Colors.blue : Colors.grey,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          products[index]["image"],
+                          height: 80,
+                          width: 80,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          products[index]["name"],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          "\$${products[index]["price"]}",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
